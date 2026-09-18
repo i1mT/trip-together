97b3dcdd6dc86f03ace9c2cfb072ee2a12f563c9
i1mT <tfhhh@qq.com>
2026-09-19 00:02:18 +0800

    fix: generate friendly share codes for legacy itineraries
    legacy share codes used an incrementing hex format that produced all-digit
    codes like 00000001; switch to the same 8-char random alphabet used for new
    publications (excluding I/O/0/1) so they are easy to type

--- infra/schema/0008_market_authors_codes.sql
+++ infra/schema/0008_market_authors_codes.sql
@@ -1,4 +1,12 @@
 ALTER TABLE public_itineraries ADD COLUMN code TEXT NOT NULL DEFAULT '';
 ALTER TABLE public_itineraries ADD COLUMN introduction TEXT NOT NULL DEFAULT '';
-UPDATE public_itineraries SET code=(SELECT printf('%08X', position) FROM (SELECT id,row_number() OVER (ORDER BY id) AS position FROM public_itineraries) numbered WHERE numbered.id=public_itineraries.id);
+UPDATE public_itineraries SET code=
+  substr('ABCDEFGHJKLMNPQRSTUVWXYZ23456789',(random() & 0x7fffffff) % 32 + 1,1) ||
+  substr('ABCDEFGHJKLMNPQRSTUVWXYZ23456789',(random() & 0x7fffffff) % 32 + 1,1) ||
+  substr('ABCDEFGHJKLMNPQRSTUVWXYZ23456789',(random() & 0x7fffffff) % 32 + 1,1) ||
+  substr('ABCDEFGHJKLMNPQRSTUVWXYZ23456789',(random() & 0x7fffffff) % 32 + 1,1) ||
+  substr('ABCDEFGHJKLMNPQRSTUVWXYZ23456789',(random() & 0x7fffffff) % 32 + 1,1) ||
+  substr('ABCDEFGHJKLMNPQRSTUVWXYZ23456789',(random() & 0x7fffffff) % 32 + 1,1) ||
+  substr('ABCDEFGHJKLMNPQRSTUVWXYZ23456789',(random() & 0x7fffffff) % 32 + 1,1) ||
+  substr('ABCDEFGHJKLMNPQRSTUVWXYZ23456789',(random() & 0x7fffffff) % 32 + 1,1);
 CREATE UNIQUE INDEX public_itineraries_code ON public_itineraries(code);
